69e051ece16968d84d24693a11292bda8f553072
Aleksey <alekseytim@mail.ru>
2022-11-15 22:19:23 +0300

    feat: finish

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../../hooks/redux";
 import { usersSlice } from "../../../store/reducers/UsersSlice";
-import { BtnExit } from "../../BtnExit/BtnExit"
+import { BtnBack } from "../../Buttons/BtnBack/BtnBack";
+import { BtnExit } from "../../Buttons/BtnExit/BtnExit"
+import { EmailIcon } from "../../Icons/EmailIcon/EmailIcon";
+import { PhoneIcon } from "../../Icons/PhoneIcon/PhoneIcon";
 
 interface IUserInterface {
   email: string,
@@ -32,34 +35,51 @@ export const User = ({ email, first_name, last_name, text, avatar }: IUserInterf
     <div>
       <header className=" bg-[var(--main-color)] mb-[48px]">
         <div
-          className="flex max-w-[1440px] m-auto items-center justify-start px-[88px] py-[32px] 
+          className="flex flex-row justify-between sm:justify-start max-w-[1480px] m-auto 
+          tems-center px-[20px] pt-[23px] sm:pt-[32px] pb-[48px] sm:pb-[32px] 
         bg-[var(--main-color)]"
         >
           <button
             onClick={handleBackBtn}
             className="outline-none rounded-[8px] self-start focus:outline-none p-[11px] 
-            sm:px-[16px] sm:py-[8px] sm:border-[#f8f8f8] mr-[29px] transition-all ease-in-out 
+            sm:px-[16px] sm:py-[8px] sm:border-[var(--border-color)] sm:mr-[29px] transition-all ease-in-out 
             duration-75 active:bg-[var(--active-color)] hover:bg-[var(--hover-color)]"
           >
             <span
               className="text-white text-[16px] leading-[22px] hidden sm:block 
             display-none transition-all ease-in-out duration-75 "
-            >Назад</span>
-            <BtnExit />
+            >
+              Назад
+            </span>
+            <BtnBack />
           </button>
-          <img
-            src={avatar}
-            alt={'User avatar'}
-            className="rounded-[50%] sm:w-[187px] sm:h-[187px] mr-[32px]"
-          />
-          <div className="flex flex-col gap-[16px]">
-            <h1 className="text-white text-[64px] leading-[75px]">{first_name}&nbsp;{last_name}</h1>
-            <p className=" text-white text-[32px] leading-[38px]">Партнер</p>
+          <div
+            className="flex flex-col-reverse items-center text-center sm:text-start gap-[16px] sm:[gap-0] sm:flex-row 
+          pt-[64px] sm:pt-0 "
+          >
+            <img
+              src={avatar}
+              alt={'User avatar'}
+              className="rounded-[50%] w-[187px] h-[187px] sm:mr-[32px]"
+            />
+            <div className="flex flex-col gap-[16px] sm:text-start">
+              <h1
+                className="text-white text-[36px] leading-[42px] lg:text-[64px] 
+              lg:leading-[75px]"
+              >
+                {first_name}&nbsp;{last_name}</h1>
+              <p
+                className=" text-white text-[20px] leading-[23px] lg:text-[32px] 
+              lg:leading-[38px]"
+              >
+                Партнер</p>
+            </div>
           </div>
           <button
             onClick={handleExit}
             className="outline-none rounded-[8px] focus:outline-none p-[11px] sm:px-[16px] 
-            sm:py-[8px] sm:border-[#f8f8f8] ml-auto mr-[3px] self-start transition-all ease-in-out 
+            sm:py-[8px] sm:border-[var(--border-color)] sm:ml-auto sm:mr-[3px] self-start 
+            transition-all ease-in-out 
             duration-75 active:bg-[var(--active-color)] hover:bg-[var(--hover-color)]"
           >
             <span
@@ -72,6 +92,39 @@ export const User = ({ email, first_name, last_name, text, avatar }: IUserInterf
           </button>
         </div>
       </header>
+      <main
+        className="flex flex-col-reverse sm:flex-row px-[10px] gap-[34px] items-center justify-center 
+      sm:gap-[130px] "
+      >
+        <div className="max-w-[630px] sm:max-h-[396px] text-[16px] leading-[22px] mb-[32px]">
+          Клиенты видят в нем эксперта по вопросам разработки комплексных решений финансовых продуктов,
+          включая такие аспекты, как организационная структура, процессы, аналитика и ИТ-компоненты.
+          Он помогает клиентам лучше понимать структуру рисков их бизнеса, улучшать процессы за счет
+          применения новейших технологий и увеличивать продажи, используя самые современные аналитические
+          инструменты.
+          <br></br>
+          <br></br>
+          В работе с клиентами недостаточно просто решить конкретную проблему или помочь справиться с трудностями.
+          Не менее важно уделять внимание обмену знаниями: "Один из самых позитивных моментов — это осознание того,
+          что ты помог клиенту перейти на совершенно новый уровень компетентности, уверенность в том, что после
+          окончания проекта у клиента есть все необходимое, чтобы дальше развиваться самостоятельно".
+          <br></br>
+          <br></br>
+          Помимо разнообразных проектов для клиентов финансового сектора, Сорин ведет активную предпринимательскую
+          деятельность. Он является совладельцем сети клиник эстетической медицины в Швейцарии, предлагающей
+          инновационный подход к красоте, а также инвестором других бизнес-проектов.
+        </div>
+        <div className="flex flex-col gap-[25px] self-start">
+          <div className="flex items-center gap-[10px]">
+            <PhoneIcon />
+            <a href="tel:+7 (954) 333-44-55" className="text-[16px] leading-[22px]">+7 (954) 333-44-55</a>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <EmailIcon />
+            <a href="email:{email}" className="text-[16px] leading-[22px]">{email}</a>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
--- src/components/Users/UsersPage/UsersPage.tsx
+++ src/components/Users/UsersPage/UsersPage.tsx
@@ -1,14 +1,16 @@
 import { GenericList } from "../../../GenericList/GenericList";
+import { useMediaQuery } from 'react-responsive'
 import { merge } from "../../../services/ts/merge";
 import { generateId, generateRandomString } from "../../../utils/generateRandomIndex";
-import { BtnExit } from "../../BtnExit/BtnExit"
+import { BtnExit } from "../../Buttons/BtnExit/BtnExit"
 import { UserCard } from "../UserCard/UserCard";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import { usersSlice } from "../../../store/reducers/UsersSlice";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchUsers } from "../../../store/reducers/ActionsCreators";
 import useToken from "../../../hooks/useToken";
+import './styles.css'
 
 export const UsersPage = () => {
 
@@ -16,14 +18,21 @@ export const UsersPage = () => {
   const { users, isLoading, error } = useAppSelector(state => state.userReducer)
   const { isLoggedSuccess } = usersSlice.actions;
   const navigate = useNavigate();
-  const { token } = useToken()
-
+  const { token } = useToken();
+  const [showMore, setShowMore] = useState(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 630px)' })
 
   useEffect(() => {
     dispatch(fetchUsers(token));
   }, [])
 
+  const numberOfUsers = showMore ? users.length : 8;
+  const numberOfUsersMobile = showMore ? users.length : 4;
 
+  const handleShowMore = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setShowMore(true);
+  }
 
   const handleExit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -32,7 +41,7 @@ export const UsersPage = () => {
     navigate('/login')
   }
 
-  const genericUsers = users?.map((user) => {
+  const genericUsers = users?.slice(0, !isMobile ? numberOfUsers : numberOfUsersMobile).map((user) => {
     const USERS = [
       {
         element: [<UserCard key={generateRandomString()} id={user?.id} email={user?.email} first_name={user?.first_name} avatar={user?.avatar} last_name={user?.last_name} />],
@@ -51,17 +60,17 @@ export const UsersPage = () => {
   })
 
   return (
-    <div>
+    <div className="mb-[69px]">
       <header className=" bg-[var(--main-color)] mb-[48px]">
         <div
-          className="flex flex-col gap-[16px] max-w-[1440px] m-auto items-center text-center pt-[23px] 
-        md:pt-[32px] pl-[20px] pr-[20px]  pb-[64px] transition-all ease-in-out duration-75 
+          className="flex flex-col gap-[16px] max-w-[1480px] m-auto items-center text-center pt-[23px] 
+        md:pt-[32px] px-[20px] pb-[64px] transition-all ease-in-out duration-75 
         active:text-[var(--main-color)] mb-[32px] sm:mb-[48px]"
         >
           <button
             onClick={handleExit}
-            className="outline-none rounded-[8px] focus:outline-none p-[11px] sm:px-[16px] 
-            sm:py-[8px] sm:border-[#f8f8f8] ml-auto mr-[3px] active:bg-[var(--active-color)] 
+            className="outline-none rounded-[8px] focus:outline-none sm:px-[16px] 
+            sm:py-[8px] sm:border-[var(--border-color)] ml-auto mr-[3px] active:bg-[var(--active-color)] 
             hover:bg-[var(--hover-color)] transition-all ease-in-out duration-75"
           >
             <span
@@ -81,13 +90,24 @@ export const UsersPage = () => {
           </p>
         </div>
       </header>
-      <main className="px-[80px] max-w-[1440px] m-auto">
+      <main className="max-w-[1440px] m-auto">
         {isLoading && <h1>Идет загрузка</h1>}
         {error && <h1>{error}</h1>}
         <ul className="flex gap-[20px] flex-wrap mb-[65px] items-center justify-center">
           {genericUsers}
         </ul>
       </main>
-    </div>
+      <button
+        onClick={handleShowMore}
+        className='flex items-center border border-[#151317] rounded-[8px] 
+        gap-[11px] m-auto px-[16px] py-[9px] hover:bg-[var(--hover-color)] hover:text-white 
+        transition-all .3s ease-in-out'
+      >
+        <span>Показать еще</span>
+        <svg className="arrow-svg" width="18" height="10" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M16.497 0.989027L8.99999 8.29703L1.50299 0.989027C1.36905 0.858193 1.18923 0.784947 1.00199 0.784947C0.814751 0.784947 0.634939 0.858193 0.500992 0.989027C0.436135 1.05257 0.384611 1.12842 0.349436 1.21213C0.314261 1.29584 0.296143 1.38573 0.296143 1.47653C0.296143 1.56733 0.314261 1.65721 0.349436 1.74092C0.384611 1.82463 0.436135 1.90048 0.500992 1.96403L8.47649 9.74003C8.61655 9.87655 8.8044 9.95295 8.99999 9.95295C9.19558 9.95295 9.38343 9.87655 9.52349 9.74003L17.499 1.96553C17.5643 1.90193 17.6162 1.8259 17.6517 1.74191C17.6871 1.65792 17.7054 1.56769 17.7054 1.47653C17.7054 1.38537 17.6871 1.29513 17.6517 1.21114C17.6162 1.12715 17.5643 1.05112 17.499 0.987526C17.365 0.856693 17.1852 0.783447 16.998 0.783447C16.8108 0.783447 16.6309 0.856693 16.497 0.987526V0.989027Z" />
+        </svg>
+      </button>
+    </div >
   )
 }
