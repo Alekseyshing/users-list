ae5a2ffed41f8b6ca79ab3428380b1198d8c76dc
Aleksey <alekseytim@mil.ru>
2025-03-30 14:55:29 +0200

    vercel

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? '/api'  // Для продакшена используем относительный путь
+  : 'http://localhost:5001/api';  // Для разработки используем локальный сервер
 
 const api = axios.create({
   baseURL: API_URL,
